fa3ba19fc24d78bc173ee7b21a111c99fa810941
rdoran1 <33032112+rdoran1@users.noreply.github.com>
2019-11-04 17:24:24 +0000

    Initial Push
    Featuring Movement, Trial Level Design, and Initial Idea

---
+++ Sloth Flee and Climb/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector3(0,1,0) * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector3(0, -1, 0) * speed;
+        }
+    }
+}
